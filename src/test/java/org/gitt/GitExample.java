de0b7b5ea9ea44863a5b44645a987396d46e7cc9
Jelina-Marshal <jelinamarshal@gmail.com>
2022-07-19 11:20:19 +0530

    Project Example

---
+++ src/test/java/org/gitt/GitExample.java
@@ -0,0 +1,10 @@
+package org.gitt;
+
+public class GitExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");//C:\Users\DELL\eclipse-workspace\GitTrial - location of workspace in windows
+	}
+
+}
