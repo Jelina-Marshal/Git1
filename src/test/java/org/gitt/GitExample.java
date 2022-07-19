9271993d383d6ce774ee6ae76b5027f2f0332c5d
Jelina-Marshal <109573157+Jelina-Marshal@users.noreply.github.com>
2022-07-19 11:57:47 +0530

    Update GitExample.java

--- src/test/java/org/gitt/GitExample.java
+++ src/test/java/org/gitt/GitExample.java
@@ -4,7 +4,7 @@ public class GitExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi");//C:\Users\DELL\eclipse-workspace\GitTrial - location of workspace in windows
+		System.out.println("hi");
 	}
 
 }
